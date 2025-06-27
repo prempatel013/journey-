7d29057ea24bfca6bcbfdc0f7daeed56d067c818
prempatel013 <rolexhere03@gmail.com>
2025-06-27 19:56:57 +0530

    the day 01 is done so day 01 is finished

---
+++ day 1/todays projects/Mad Libs Generator.py
@@ -0,0 +1,3 @@
+adjective=input('enter the adjective: ')
+Noun=input('enter the noun: ')
+print(f"The {adjective} {Noun} quickly caught everyone's attention.")
--- day 1/todays projects/Unit Converter.py
+++ day 1/todays projects/Unit Converter.py
@@ -2,3 +2,6 @@
 #  here this is project is to convert units from one to another
 #  this is a simple unit converter that can convert between different units of measurement
 
+mile=float(input("Enter the miles to convert to Km's: "))
+km=0.6214*mile
+print(f'the kilometer is {km}')

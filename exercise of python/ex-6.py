2353bc2ace12a31929ab9e75dc8143f8d4346f63
Prem Patel <rolexhere03@gmail.com>
2025-07-02 07:57:34 +0530

    excercise 6 is done

--- exercise of python/ex-6.py
+++ exercise of python/ex-6.py
@@ -10,3 +10,16 @@
 # D (60-69)
 
 # F (<60)
+
+grade=input("Enter your grade: ")
+
+if grade>=90:
+   print('you got A')
+elif grade>=80:
+   print('B')
+elif grade>=70:
+   print('C')
+elif grade>=60:
+   print('D')
+elif grade>60:
+   print('F')

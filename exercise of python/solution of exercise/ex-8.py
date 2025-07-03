a18b1f3ac048769eaa743b64f062b924b856182a
prempatel013 <rolexhere03@gmail.com>
2025-07-03 13:48:37 +0530

    the ex 8 is over

---
+++ exercise of python/solution of exercise/ex-8.py
@@ -0,0 +1,18 @@
+# Exercise 8: FizzBuzz (Advanced if/else & loop)
+# Print numbers from 1 to 20, but:
+
+# If a number is divisible by 3, print "Fizz".
+
+# If divisible by 5, print "Buzz".
+
+# If divisible by both, print "FizzBuzz".
+
+for i in range(1,21):
+    
+    if i % 3 == 0:
+        print(f"{i} is divisible by 3 : Fizz ")
+    elif i%5 ==0:
+        print(f"{i} is divisible by 5: Buzz")
+        
+    elif  i % 3 == 0 and i%5 ==0:
+        print(f"{i} is divisible by both: FizzBuzz")

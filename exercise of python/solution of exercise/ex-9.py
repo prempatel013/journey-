cb9cc5f9b6ad9e7ddf74852e79261d66de70ee86
prempatel013 <rolexhere03@gmail.com>
2025-07-03 13:51:26 +0530

    ex 9 is over

---
+++ exercise of python/solution of exercise/ex-9.py
@@ -0,0 +1,8 @@
+
+# Exercise 9: Reverse a String (Loop Practice)
+# Ask the user for a string and print it in reverse using a loop.
+
+
+str=input("enter the string('like Hello'): ")
+reversed_str=str[::-1]
+print(reversed_str)
